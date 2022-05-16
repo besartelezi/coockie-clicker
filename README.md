f86d2588f2adb28c01dc3d1788890af20225f6d4
Besart Elezi <besart.elezi@hotmail.com>
2022-05-16 16:52:44 +0200

    fixed the multiplier so it grows steadier

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 With the power of programming and childrens cartoons on my side, I have created a cookie clicker with my own fun little twist on it. </br>
 
 #### [Click here to play the Krusty Krab Click Game](https://besartelezi.github.io/coockie-clicker/)
+
--- cookie-clicker.js
+++ cookie-clicker.js
@@ -7,7 +7,7 @@
     //The Variable of the amount of clicks on start, the value of each click and the value of the multiplier
     let ClickAmount = 0;
     let Clicks = 1;
-    let ClickMultiplier = 2;
+    let ClickMultiplier = 1;
 
     //The different upgrades you can buy at the Krusty Krab
     let GoldenSpatula = 5;
@@ -25,12 +25,15 @@
 
     //variable of the button that will be used for the multiplier
     const Multiply = document.getElementById("multiplier");
-    //Multiply Function, it starts at a cost of 5, but the cost will increase by 3 times on every purchase
+    //Multiply Function, it starts at a cost of 5, but the cost will increase by 3 times on every purchase. Every time the button is used it multiplies the clicks by 2
     const Multipliedby2ButtonClicks = () => {
         //If the user has less than 5 clicks, the button will not work.
         if (ClickAmount >= GoldenSpatula) {
             ClickAmount -= GoldenSpatula;
             Counter.innerHTML = ClickAmount;
+            //In order for this to keep growing exponentially, I started off by dividing the current clicks by the current multiplier, THEN letting the multiplier grow by '1', THEN using the multiplier on the Clicks.
+            Clicks /= ClickMultiplier;
+            ClickMultiplier++;
             Clicks *= ClickMultiplier;
             GoldenSpatula *= 3;
         }
