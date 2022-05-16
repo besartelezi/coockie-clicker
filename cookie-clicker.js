679f7746aa9c70183fb9c4e8f92e568b1e0c9f55
Besart Elezi <besart.elezi@hotmail.com>
2022-05-16 16:46:53 +0200

    updated the multiplier, still need to decrease the amount of clicks it multiplies by

--- cookie-clicker.js
+++ cookie-clicker.js
@@ -3,11 +3,14 @@
     //Selection of the elements in the HTML that will be used in javascript
     const Counter = document.getElementById("BurgerCounter");
     const Button = document.getElementById("KrabbyPatty");
-    const Multiply = document.getElementById("multiplier");
 
-    //Variables that ...
+    //The Variable of the amount of clicks on start, the value of each click and the value of the multiplier
     let ClickAmount = 0;
     let Clicks = 1;
+    let ClickMultiplier = 2;
+
+    //The different upgrades you can buy at the Krusty Krab
+    let GoldenSpatula = 5;
 
     //Showing the current amount of clicks on the website
     Counter.innerHTML = ClickAmount;
@@ -19,16 +22,21 @@
     }
     Button.addEventListener("click", ButtonClicks);
 
-    //Multiply Function, this will increase the click-amount by 2, but the user needs to pay 5 clicks to use it
+
+    //variable of the button that will be used for the multiplier
+    const Multiply = document.getElementById("multiplier");
+    //Multiply Function, it starts at a cost of 5, but the cost will increase by 3 times on every purchase
     const Multipliedby2ButtonClicks = () => {
         //If the user has less than 5 clicks, the button will not work.
-        if (ClickAmount >= 5) {
-            ClickAmount -= 5;
+        if (ClickAmount >= GoldenSpatula) {
+            ClickAmount -= GoldenSpatula;
             Counter.innerHTML = ClickAmount;
-            Clicks *= 2;
+            Clicks *= ClickMultiplier;
+            GoldenSpatula *= 3;
         }
     }
     Multiply.addEventListener("click", Multipliedby2ButtonClicks);
+
 
 
 })();
