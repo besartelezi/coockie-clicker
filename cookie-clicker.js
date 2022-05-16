51c49c63ca387e80e22e7b1a2dd68c69f582b0a3
Besart Elezi <besart.elezi@hotmail.com>
2022-05-16 11:00:06 +0200

    basic cookie clicker is finished, the amount shown on the screen increases on button clicks

--- cookie-clicker.js
+++ cookie-clicker.js
@@ -1,16 +1,16 @@
 (function () {
 
+    //Selecting and naming the variables that are used in the HTML, while also adding the variable amount of clicks
+    const Counter = document.getElementById("PunchCounter");
+    const Button = document.getElementById("DioBrando");
+    let ClickAmount = 0;
+    Counter.innerHTML = ClickAmount;
 
-    let ClickCounter = [0];
-    const ClickLabel = document.getElementById("oras");
-    ClickLabel.innerHTML = ClickCounter
-
-
-    const Clicker = document.getElementById("DioBrando").addEventListener("click", ButtonClick);
-    function ButtonClick () {
-        ClickCounter.value + 1;
+    //Some badabing badaboom
+    const ButtonClicks = () => {
+        ClickAmount++;
+        Counter.innerHTML = ClickAmount
     }
-
-
+    Button.addEventListener("click", ButtonClicks)
 
 })();
