406406fc67475c61d037181d4f833e7cb4b0c5dc
Besart Elezi <besart.elezi@hotmail.com>
2022-05-16 11:44:50 +0200

    rewrote code that added a value to amount of clicks, making it easier/possible to use the multiplication button once it has been implemented

--- cookie-clicker.js
+++ cookie-clicker.js
@@ -1,16 +1,17 @@
 (function () {
 
-    //Selecting and naming the variables that are used in the HTML, while also adding the variable amount of clicks
+    //Selecting and naming the variables that are used in the HTML, while also adding the starting variable for the amount of clicks
     const Counter = document.getElementById("PunchCounter");
     const Button = document.getElementById("DioBrando");
     let ClickAmount = 0;
     Counter.innerHTML = ClickAmount;
+    let Clicks = 1;
+    let Multiplier = 2;
 
-    //Some badabing badaboom
+    //Clicking function, on click increase amount in label by 1
     const ButtonClicks = () => {
-        ClickAmount++;
-        Counter.innerHTML = ClickAmount
+        ClickAmount += Clicks ;
+        Counter.innerHTML = ClickAmount;
     }
     Button.addEventListener("click", ButtonClicks)
-
 })();
